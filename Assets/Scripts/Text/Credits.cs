0a2f9e2314dc3dd1dcdb002250ff9226a4c1a7e5
Seth8000 <hansnicholasiversen@gmail.com>
2019-10-05 10:53:25 +0200

    Added scripts

---
+++ Assets/Scripts/Text/Credits.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
+    public void CreditsButton()
+    {
+        MainMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+}
